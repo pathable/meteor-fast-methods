0ed5a089b4fd97dddd20276c9d110531b8722b34
Renan Castro <renanccastro@gmail.com>
2020-07-28 19:22:11 -0300

    Use EJSON for emitting events, adding impersonate collection option, and other fixes

--- client.js
+++ client.js
@@ -5,29 +5,40 @@ import './lib/override-mongo-behavior';
 import { CHANGES_METHOD_NAME, fillMiniMongo } from './lib/helpers';
 import { Mongo } from 'meteor/mongo';
 import { Tracker } from 'meteor/tracker';
+import { EJSON } from 'meteor/ejson';
 import './lib/auth';
+
 FastMethods = {
   readyDependency: new Tracker.Dependency(),
   isReady: false,
-  ready: function () {
+  ready() {
     this.readyDependency.depend();
     return this.isReady;
   },
+  handlers: [],
   initiallyLoggedIn: false,
-  startListeningToChanges: ({ namespace }) => {
+  startListeningToChanges({ namespace, methodName = CHANGES_METHOD_NAME }) {
     import { Vent } from 'meteor/cultofcoders:redis-oplog';
 
-    const handler = Vent.subscribe(CHANGES_METHOD_NAME, { namespace });
+    const handler = Vent.subscribe(methodName, { namespace });
     const getCollectionByName = Mongo.Collection.__getCollectionByName;
-    handler.listen(({ doc, docId, e: event, n: collectionName }) => {
+    handler.listen(({ doc: docString, docId, e: event, n: collectionName }) => {
+      const collectionByName = getCollectionByName(collectionName);
       if (event === 'r') {
-        getCollectionByName(collectionName).rawRemove(docId);
+        collectionByName.rawRemove(docId);
         return;
       }
-      getCollectionByName(collectionName).set(doc?._id, { ...doc, __noReset: true });
+      const doc = EJSON.parse(docString);
+      collectionByName.set(doc?._id, {
+        ...doc,
+        __noReset: true,
+      });
     });
+    this.handlers.push(handler);
+    return handler;
   },
 };
+
 Meteor.startup(() => {
   fillMiniMongo();
 });
